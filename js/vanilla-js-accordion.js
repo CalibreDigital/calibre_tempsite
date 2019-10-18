de590e2f1f42a8b25c83286dbc217029d45e0a25
jerrysam <jeromeminney@gmail.com>
2019-10-18 17:12:06 +0100

    finish PFDT case study

--- js/vanilla-js-accordion.js
+++ js/vanilla-js-accordion.js
@@ -91,7 +91,7 @@ var Accordion = function(options) {
             el.dataset.titlecache = el.innerHTML;
             el.style.opacity = "0";
             setTimeout(function(){ 
-                el.innerHTML = "Collapse this section and continue reading"; 
+                el.innerHTML = "Collapse this section"; 
                 el.style.opacity = "1";
             }, 600);
             
